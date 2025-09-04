348b220a0786f95463f800edc91bfd0b056a6760
FLOYD <sungmin.park5941@gmail.com>
2025-09-04 09:49:43 +0900

    Spring Core 33 ~ 35 템플릿 콜백 패턴 패턴 예제 및 적용

---
+++ src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,33 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.LogTrace;
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate template;
+
+    //생성시점에 주입
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace trace) {
+        this.orderService = orderService;
+        this.template = new TraceTemplate(trace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        //템플릿 콜백 패턴 적용
+        return template.execute("OrderController.request()", new TraceCallback<>() {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        });
+    }
+}
